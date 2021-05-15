ca00f83af92e6a7b8fd21c4c76e36134af4e7300
narcode <contact@felipeignacio.info>
2021-05-15 13:03:31 +0200

    iniital commit

---
+++ read_json.py
@@ -0,0 +1,14 @@
+# read json in
+
+import json
+
+answers = '/Users/narcodeb/Downloads/artists.json'
+questions = '/Users/narcodeb/Downloads/questions_short.json'
+a = open(answers)
+q = open(questions)
+
+ja = json.load(f)
+jq = json.load(q)
+
+
+print(j)
